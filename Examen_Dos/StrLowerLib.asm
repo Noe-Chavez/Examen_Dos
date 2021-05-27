50fdde6fdf960186597490cbeae95ba5c244b44e
Chávez Morelos Noé Baldemar <noe_fam_chavez@yahoo.com.mx>
2021-05-27 18:44:28 -0500

    Función que convierte una cadena de mayusculas a minusculas, siempre y cuando la cadena este en minusculas y son caracteres distintos a letras.

---
+++ Examen_Dos/StrLowerLib.asm
@@ -0,0 +1,45 @@
+	.386
+	.MODEL flat, C
+
+	.STACK 2560
+
+	.DATA
+
+	temCad DWORD 10 DUP (0)
+
+	.CODE
+
+	; Funcion que conbvierte una cadena a mayusuclas
+	; int* StrUpper(char *cad)
+StrLower PROC, pCad:DWORD
+
+	PUSH ECX
+	PUSH ESI
+	
+	MOV ECX, 0				; i = 0
+	MOV ESI, [pCad]			; apuntar (p) al primer elemento de pCad[0] (ESI <-- [pCad])
+
+Ini_For:
+
+	MOV AL, [ESI]			; leemos el primer caracter de la cadena AL<--pCad[i]
+	CMP AL, 0				; AL - 0 (resta)
+	JE	Fin_For				; Si z = 0, entonces ya se acabo la cadena, por lo que saltamos a Fin_For  
+	; SI Z /= 0, entonces entramos al cuerpo del for.
+	ADD AL, 20h				; 20h = 32 decimal
+	MOV [ESI], AL			; pCad[i] = AL su equivalente en mayuscula
+	INC EDI					; incremetntamos el apuntador pCad
+	INC ECX					; i <-- i + 1
+	INC ESI					; movemos el apuntador (p)
+	JMP Ini_For				; volvemos a realizar otro ciclo, hasta que z = 1 
+
+Fin_For:
+	;MOV EAX, [pCad]			; return pCad  
+	
+	POP ESI
+	POP ECX
+	
+	RET 
+
+StrLower ENDP
+
+	END
--- Examen_Dos/main.cpp
+++ Examen_Dos/main.cpp
@@ -7,18 +7,22 @@ using namespace std;
 
 extern "C" int StrLength(char* cad);
 extern "C" void StrUpper(char* cad);
-//extern "C" void StrLower(char* cad);
+extern "C" void StrLower(char* cad);
 
 int main()
 {
-    char cad[] = "hola mundo";
-    //char cad2[] = "HOLA MUNDO";
+    char cad[] = "holamundo";
+    char cad2[] = "HOLAMUNDO";
     
-    cout << "1.a) Obtiene el numero de caracteres que tiene la cadena." << endl;
+    cout << "1) Obtiene el numero de caracteres que tiene la cadena." << endl;
     cout <<  "La cadena tiene " << StrLength(cad) << " caracteres" << endl;
 
-    cout << "2.a) Convierte a mayusculas una cadena." << endl;
+    cout << "2) Convierte a mayusculas una cadena." << endl;
     StrUpper(cad);
     cout << "La cadena en mayusculas es: " << cad << endl;
+
+    cout << "3) Conviernte a minusculas una cadena" << endl;
+    StrLower(cad2);
+    cout << "La cadena a mayusculas es: " << cad2 << endl;
     
 }
