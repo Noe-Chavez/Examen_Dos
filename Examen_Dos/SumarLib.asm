472384240504b9bc877331ec4186c8567981d535
Chávez Morelos Noé Baldemar <noe_fam_chavez@yahoo.com.mx>
2021-06-03 03:22:16 -0500

    No realiza bien la suma.

--- Examen_Dos/SumarLib.asm
+++ Examen_Dos/SumarLib.asm
@@ -10,7 +10,27 @@
 	; Funcion que suma los elementos del vector 
 	; int Sumar(int *lista, int N);
 Sumar PROC, pArray:DWORD, numElem:DWORD
-	
+	PUSH ESI
+	PUSH ECX
+	PUSH EDX
+	MOV ESI, [pArray]		; cargando el primer elemento del arreglo al registro CBX
+	MOV ECX, 0				; i = 0
+	MOV EAX, 0
+	MOV EDX, numElem		; cantidad de elementos en el arreglo
+ciclo:
+	CMP ECX, EDX			; compara ECX (i) y EDX (numElement)
+	JE fin_ciclo			; sí ECX == EDX, se sale del ciclo
+	MOV EAX, [ESI]			; leemos el primer elemento del array
+	ADD EAX, [ESI + 8]
+	INC ESI					; movemos el apuntador (pArray)
+	INC ECX					; i++
+	JMP ciclo
+fin_ciclo:
+	; Finalizar ciclo
+	; para regresar el valor se deben colocar en el registro EAX
+	POP EDX
+	POP ECX
+	POP ESI
+	RET
 Sumar ENDP
-
 	END
